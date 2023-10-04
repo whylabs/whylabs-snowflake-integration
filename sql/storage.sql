a0232936bc0d13bc127b8c0f9aa79c0918382a77
Anthony Naddeo <anthony@whylabs.ai>
2023-10-04 14:32:43 -0700

    Increase version no, add install instructions to release

--- sql/storage.sql
+++ sql/storage.sql
@@ -4,14 +4,13 @@ CREATE OR REPLACE STORAGE INTEGRATION whylabs_s3_integration
   STORAGE_PROVIDER = 'S3'
   ENABLED = TRUE
   STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::207285235248:role/public-snowflake-role'
-  STORAGE_ALLOWED_LOCATIONS = ('s3://guest-session-testing-public/udfs/')
+  STORAGE_ALLOWED_LOCATIONS = ('s3://whylabs-snowflake-udfs/udfs/')
 ;
---   STORAGE_BLOCKED_LOCATIONS = ();
 
 
-CREATE OR REPLACE STAGE my_s3_stage
+CREATE OR REPLACE STAGE whylabs_udf_stage
   STORAGE_INTEGRATION = whylabs_s3_integration
-  URL = 's3://guest-session-testing-public/udfs/'
+  URL = 's3://whylabs-snowflake-udfs/udfs/'
 ;
 
 
@@ -30,5 +29,5 @@ create or replace function whylogs_public(data object)
     secrets = ('data_grouper_freq' = data_grouper_freq, 'segment_columns' = segment_columns)
     packages = ('snowflake-snowpark-python', 'whylogs', 'pandas')
     handler = 'whylogs_udf.handler'
-    imports = ('@my_s3_stage/whylogs_udf.py')
+    imports = ('@whylabs_udf_stage/v1/latest/whylogs_udf.py')
     ;
